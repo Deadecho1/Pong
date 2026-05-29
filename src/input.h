5a555f7e2deabce08fe1193dd6ff9550eb631b93
Deadecho1 <167852254+Deadecho1@users.noreply.github.com>
2026-05-29 11:31:23 +0300

    feat: input system
    input.h:-
    unified input class for controller and keyboard inputs. handles one player's input. uses input state polling.
    
    main.cpp-
    now contains small demo of usage

---
+++ src/input.h
@@ -0,0 +1,128 @@
+#ifndef __INPUT_H__
+#define __INPUT_H__
+
+#include <SDL3/SDL.h>
+
+class Input
+{
+public:
+    struct InputState {
+        bool up;
+        bool down;
+        bool confirm;
+        bool esc;
+    };
+
+    struct KeyboardConfig
+    {
+        SDL_Scancode up;
+        SDL_Scancode down;
+        SDL_Scancode confirm;
+        SDL_Scancode esc;
+    };
+
+    // consts
+    static constexpr float DEADZONE = 2000.0f;
+    
+    Input(const KeyboardConfig &initialConfig);
+
+    ~Input();
+
+    void SetKeyboardInputConfig(const KeyboardConfig &config);
+
+    // gets a const reference to the controller
+    const SDL_Gamepad* GetController(); 
+
+    // associates a controller with this input.
+    void SetController(SDL_Gamepad* gamepad);
+
+    // call to disassociate controller with input. returns the controller.
+    SDL_Gamepad* RemoveController();
+
+    // call in game loop
+    bool InputStatePoll(InputState& inputOut);
+
+    bool IsActive();
+
+    void SetActive(bool state);
+
+private:
+    SDL_Gamepad *mGamepad = nullptr;
+
+    KeyboardConfig mConfig;
+
+    bool mIsActive = false;
+};
+
+
+Input::Input(const KeyboardConfig &initialConfig)
+{
+    SetKeyboardInputConfig(initialConfig);
+}
+
+Input::~Input()
+{
+}
+
+const SDL_Gamepad* Input::GetController()
+{
+    return mGamepad;
+}
+
+void Input::SetKeyboardInputConfig(const KeyboardConfig &config)
+{
+    mConfig = config;
+}
+
+void Input::SetController(SDL_Gamepad* gamepad)
+{
+    mGamepad = gamepad;
+}
+
+SDL_Gamepad* Input::RemoveController()
+{
+    SDL_Gamepad *g = mGamepad;
+    mGamepad = nullptr;
+    return g;
+}
+
+bool Input::IsActive(){
+    return mIsActive;
+}
+
+void Input::SetActive(bool state){
+    mIsActive = state;
+}
+
+// returns false if input is inactive
+bool Input::InputStatePoll(Input::InputState& inputOut)
+{
+    if (!mIsActive){
+        return false;
+    }
+
+    // gamepad associated with this input
+    if (mGamepad != nullptr)
+    {         
+        inputOut.esc = SDL_GetGamepadButton(mGamepad, SDL_GAMEPAD_BUTTON_START);
+        inputOut.confirm = SDL_GetGamepadButton(mGamepad, SDL_GAMEPAD_BUTTON_SOUTH);
+
+        Sint16 axis_y = SDL_GetGamepadAxis(mGamepad, SDL_GAMEPAD_AXIS_LEFTY);
+        inputOut.down = (axis_y > Input::DEADZONE || SDL_GetGamepadButton(mGamepad, SDL_GAMEPAD_BUTTON_DPAD_UP));
+        inputOut.up = (axis_y < -Input::DEADZONE || SDL_GetGamepadButton(mGamepad,SDL_GAMEPAD_BUTTON_DPAD_DOWN));
+    }
+    // no gamepad, default to keyboard controls
+    else
+    {
+        const bool *key_states = SDL_GetKeyboardState(NULL);
+
+        inputOut.esc = (key_states[mConfig.esc]);
+        inputOut.confirm = (key_states[mConfig.confirm]);
+        inputOut.up = (key_states[mConfig.up]);
+        inputOut.down = (key_states[mConfig.down]);
+    }
+    
+    return true;
+}
+
+#endif // __INPUT_H__
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,121 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
+#include <unordered_map>
+#include <vector>
+#include "input.h"
 
 constexpr int WINDOW_WIDTH = 640;
 constexpr int WINDOW_HEIGHT = 480;
 
-static SDL_Window *window = NULL;
-static SDL_Renderer* renderer = NULL;
+constexpr int MAX_PLAYER_CONTROLLERS = 2;
+
+const Input::KeyboardConfig PLAYER1_CONFIG = {
+    SDL_SCANCODE_W, //UP
+    SDL_SCANCODE_S, //DOWN
+    SDL_SCANCODE_RETURN, //confirm
+    SDL_SCANCODE_ESCAPE, // esc
+};
+
+const Input::KeyboardConfig PLAYER2_CONFIG = {
+    SDL_SCANCODE_UP, //UP
+    SDL_SCANCODE_DOWN, //DOWN
+    SDL_SCANCODE_RETURN, //confirm
+    SDL_SCANCODE_ESCAPE, // esc
+};
+
+Input::InputState gPlayer1InputState;
+Input::InputState gPlayer2InputState;
+
+static SDL_Window *gWindow = NULL;
+static SDL_Renderer* gRenderer = NULL;
+
+static std::vector<Input*> gActiveInputs;
+static std::unordered_map<SDL_JoystickID, Input*> gGamepadToInput;
+
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[])
 {
     SDL_SetAppMetadata("Pong", "dev-0.1", "com.github.Deadecho1.Pong");
 
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMEPAD)) {
         SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
 
-    if (!SDL_CreateWindowAndRenderer("Pong", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &window, &renderer)) {
+    if (!SDL_CreateWindowAndRenderer("Pong", WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE, &gWindow, &gRenderer)) {
         SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
         return SDL_APP_FAILURE;
     }
-    SDL_SetRenderLogicalPresentation(renderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+    SDL_SetRenderLogicalPresentation(gRenderer, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+    // create space for inputs
+    gActiveInputs.reserve(MAX_PLAYER_CONTROLLERS);
+    // create player 1
+    Input* player1 = new Input(PLAYER1_CONFIG);
+    gActiveInputs.push_back(player1);
+    player1->SetActive(true);
+
+    Input* player2 = new Input(PLAYER2_CONFIG);
+    gActiveInputs.push_back(player2);
+    player2->SetActive(false);
 
     return SDL_APP_CONTINUE;
 }
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 {
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS; // end program
+    switch (event->type)
+    {
+        case SDL_EVENT_QUIT: {
+            return SDL_APP_SUCCESS; // end program
+            break;
+        }
+
+        case SDL_EVENT_GAMEPAD_ADDED: {
+            const SDL_JoystickID which = event->gdevice.which;
+            // find input with no associated gamepad
+            int inputI = 0;
+            while (inputI < gActiveInputs.size()){
+                if (gActiveInputs[inputI]->GetController() == nullptr){
+                    break;
+                }
+                inputI++;
+            }
+
+            // there is an input slot for the controller, connect it.
+            if (inputI < gActiveInputs.size()){
+                SDL_Gamepad *newController = SDL_OpenGamepad(which);
+                if (!newController){
+                    SDL_Log("Couldn't open gamepad #%u: %s", (unsigned int) which, SDL_GetError());
+                }
+                // controller opened and ready to be associated with an input
+                else{
+                    gGamepadToInput[which] = gActiveInputs[inputI];
+                    gActiveInputs[inputI]->SetController(newController);
+                }
+            }
+            break;
+        }
+
+        case SDL_EVENT_GAMEPAD_REMOVED: {
+            const SDL_JoystickID which = event->gdevice.which;
+            
+            // find if gamepad was associated wih an input
+            auto it = gGamepadToInput.find(which);
+            if (it != gGamepadToInput.end()){
+                SDL_Gamepad *gamepad = gGamepadToInput[which]->RemoveController();
+                if (gamepad){
+                    SDL_CloseGamepad(gamepad);
+                }
+            }
+
+            break;
+        }
+        
+        default: {
+            break;
+        }
     }
 
     return SDL_APP_CONTINUE;
@@ -38,18 +124,41 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 SDL_AppResult SDL_AppIterate(void *appstate)
 {
     // render black background
-    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
-    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0x00, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(gRenderer);
 
-    SDL_RenderPresent(renderer);
+    SDL_RenderPresent(gRenderer);
 
+    // poll player 1 input
+    if (gActiveInputs[0]->InputStatePoll(gPlayer1InputState)){
+        if (gPlayer1InputState.up){
+            SDL_Log("Up!");
+        }
+        if (gPlayer1InputState.down){
+            SDL_Log("Down!");
+        }
+        if (gPlayer1InputState.confirm){
+            SDL_Log("Confirm!");
+        }
+        if (gPlayer1InputState.esc){
+            SDL_Log("Escape!");
+        }
+    }
+    
     return SDL_APP_CONTINUE;
 }
 
 void SDL_AppQuit(void *appstate, SDL_AppResult result){
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    // remove inputs
+    for (int i = 0; i < gActiveInputs.size(); i++){
+        if (gActiveInputs[i] != nullptr){
+            delete gActiveInputs[i];
+        }
+    }
 
-    renderer = NULL;
-    window = NULL;
+    SDL_DestroyRenderer(gRenderer);
+    SDL_DestroyWindow(gWindow);
+
+    gRenderer = NULL;
+    gWindow = NULL;
 }
